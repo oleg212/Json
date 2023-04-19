81fe6bf05e298f406a9ef0c34d035c970a1c5ebb
oleg212 <ipnnru@gmail.com>
2023-04-19 21:48:42 +0300

    new parser

--- StatLibJson/Json.h
+++ StatLibJson/Json.h
@@ -23,6 +23,7 @@ public:
 		}
 	}
 	void AddNext(Value* _next) {
+		if (cur == nullptr) { cur = _next; return; }
 		cur->setnext(_next);
 		GoNext();
 	}
@@ -85,85 +86,161 @@ public:
 	}
 	friend istream& operator>>(istream& istr, Json& json)
 	{
+		//string temp;
+		//int depth = 1;
+		//string key = "";
+		//string val = "";
+		//int type; //1 - string, 2 - int, 3 - array
+		//bool state = 1; // мы считываем либо ключ либо значение
+		//char cur = '0';
+		//bool rb = 0;	//ждет ли массив первое значение
+		//getline(istr, temp);
+
+		//getline(istr, temp);
+
+		//key = json.get_key(temp);
+		//if (temp.find('{') != -1) {
+		//	type = 3;
+
+		//	json.Root = (new ValueArr(key, depth));
+		//	depth++;
+		//	rb = 1;
+		//}
+		//else {
+		//	val = json.get_value(temp);
+		//	if (val.find('"') == val.size()) {
+		//		type = 2;
+		//		json.Root = (new ValueInt(key, depth));
+		//		json.Root->SetContent(val);
+		//		rb = 0;
+
+		//	}
+		//	else {
+		//		type = 1;
+		//		json.Root = (new ValueStr(key, depth));
+
+		//		json.Root->SetContent(val);
+		//		rb = 0;
+		//	}
+		//}
+		//json.p = new Pointer(json.Root);
+		//while (istr) {
+
+		//	getline(istr, temp);
+		//	if (temp.find('}') != -1) {
+		//		depth--;
+		//		if (json.p->isempty())break;
+		//		json.p->GoUp(); continue;
+
+		//	};
+		//	key = json.get_key(temp);
+
+		//	if (temp.find('{') != -1) {
+		//		type = 3;
+
+		//		if (rb == 0)json.p->AddNext(new ValueArr(key, depth));
+		//		else json.p->AddRight(new ValueArr(key, depth));
+		//		rb = 1;
+		//		depth++;
+		//	}
+		//	else {
+		//		val = json.get_value(temp);
+		//		if (val.find('"') == -1) {
+		//			type = 2;
+		//			if (rb == 0) json.p->AddNext(new ValueInt(key, depth));
+		//			else json.p->AddRight(new ValueInt(key, depth));
+		//			json.p->SetContent(stoi(val));
+		//			rb = 0;
+
+		//		}
+		//		else {
+		//			type = 1;
+		//			if (rb == 0)json.p->AddNext(new ValueStr(key, depth));
+		//			else json.p->AddRight(new ValueStr(key, depth));
+		//			json.p->SetContent(val);
+		//			rb = 0;
+		//		}
+		//	}
+		//}
+		string file = "";
 		string temp;
-		int depth = 1;
-		string key = "";
-		string val = "";
-		int type; //1 - string, 2 - int, 3 - array
-		bool state = 1; // мы считываем либо ключ либо значение
-		char cur = '0';
-		bool rb = 0;	//ждет ли массив первое значение
-		getline(istr, temp);
-
-		getline(istr, temp);
-
-		key = json.get_key(temp);
-		if (temp.find('{') != -1) {
-			type = 3;
-
-			json.Root = (new ValueArr(key, depth));
-			depth++;
-			rb = 1;
-		}
-		else {
-			val = json.get_value(temp);
-			if (val.find('"') == val.size()) {
-				type = 2;
-				json.Root = (new ValueInt(key, depth));
-				json.Root->SetContent(val);
-				rb = 0;
-
-			}
-			else {
-				type = 1;
-				json.Root = (new ValueStr(key, depth));
-
-				json.Root->SetContent(val);
-				rb = 0;
-			}
-		}
-		json.p = new Pointer(json.Root);
 		while (istr) {
-
 			getline(istr, temp);
-			if (temp.find('}') != -1) {
-				depth--;
-				if (json.p->isempty())break;
-				json.p->GoUp(); continue;
-
-			};
-			key = json.get_key(temp);
-
-			if (temp.find('{') != -1) {
-				type = 3;
-
-				if (rb == 0)json.p->AddNext(new ValueArr(key, depth));
-				else json.p->AddRight(new ValueArr(key, depth));
-				rb = 1;
-				depth++;
+			file += temp;
+		}
+		//string* tokens;
+		int l = file.length();
+		int type; //1 - string, 2 - int, 3 - array
+		int depth = 1;
+		int i = 1;
+		bool rb = 0;
+		bool first = 1;
+		string s;
+		while (i<l-1) {
+			if ((file[i] != ',')&&((file[i] != '{')) && ((file[i] != '}'))) {
+				s += file[i];
 			}
 			else {
-				val = json.get_value(temp);
-				if (val.find('"') == -1) {
-					type = 2;
-					if (rb == 0) json.p->AddNext(new ValueInt(key, depth));
-					else json.p->AddRight(new ValueInt(key, depth));
-					json.p->SetContent(stoi(val));
-					rb = 0;
-
+				if ((s == "")||(s.find('"')==-1)) {
+					i++;
+					continue;
+				}
+				if (file[i] == '{') {
+					s += file[i];
+					Value* newvalue = json.process(s, depth);
+					if (rb) {
+						json.p->AddRight(newvalue);
+					}
+					else {
+						if (first) json.Root = newvalue;
+						json.p->AddNext(newvalue);
+					}
+					depth++;
+					rb = 1;
 				}
 				else {
-					type = 1;
-					if (rb == 0)json.p->AddNext(new ValueStr(key, depth));
-					else json.p->AddRight(new ValueStr(key, depth));
-					json.p->SetContent(val);
-					rb = 0;
+					Value* newvalue = json.process(s, depth);
+					if (rb) { json.p->AddRight(newvalue);rb = 0; }
+					else { json.p->AddNext(newvalue); 
+					if (first) json.Root = newvalue;
+					}
+					
 				}
+				if (file[i] == '}') {
+					json.p->GoUp();
+ 					depth--;
+				}
+				s.clear();
+				first = 0;
 			}
+			i++;
+
 		}
 
 
 		return istr;
+	}
+	Value* process(string s, int depth) {
+		string name = get_key(s);
+		if (s.find('{') != -1) {
+			Value* res = new ValueArr(name, depth);
+			return res;
+		}
+		else {			
+			string value = get_value(s);
+			if (s.find('"') == -1) {
+				Value* res = new ValueInt(name, depth);
+				res->SetContent(value);
+				return res;
+			}
+			else {
+				Value* res = new ValueStr(name, depth);
+				res->SetContent((value));
+				return res;
+			}
+
+		}
+		
 	}
 	Value* GetCur() { return p->GetCur(); }
 	void GoNext() {
